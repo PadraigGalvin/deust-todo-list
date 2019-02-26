d952674988dc184cd814fd8ec38ab9cc5195c768
Pádraig Galvin <padraig@agence-b.fr>
2019-02-26 21:06:49 +0100

    Create new Node.js project and add some bolierplate

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const port = 3000;
+const app = express();
+
+// Configure views
+app.set('view engine', 'hbs');
+app.set('view options', { layout: 'layout' });
+
+// Transform text in request body to JS objects
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Make files in public folder accessible from outside
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send('Hello, world');
+});
+
+// Error handlers
+app.use((req, res, next) => {
+  res.status(404).send('Not found');
+});
+
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).send('Internal server error');
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
