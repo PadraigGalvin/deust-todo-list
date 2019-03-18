1a27b4d5567d67138c6a14e58a76878848253cce
Pádraig Galvin <padraig@agence-b.fr>
2019-03-18 11:12:24 +0100

    Create request handlers for edit, delete, mark as (not) done

--- routes/tasks.js
+++ routes/tasks.js
@@ -11,7 +11,10 @@ router.get('/', (req, res) => {
 
 // show create task form
 router.get('/create', (req, res) => {
-  res.render('tasks/form');
+  res.render('tasks/form', {
+    title: 'Create task',
+    task: { description: '' },
+  });
 });
 
 // submit create task from
@@ -24,9 +27,53 @@ router.post('/create', (req, res) => {
   res.redirect('/tasks');
 });
 
+// load edit form task
+router.get('/edit/:id', (req, res) => {
+  let task = tasks.find((i) => i.id === parseInt(req.params.id, 10));
+  if (!task) {
+    return res.status(404).send('Not found');
+  }
+  res.render('tasks/form', { task, title: 'Edit task' });
+});
+
+// save changes to task
+router.post('/edit/:id', (req, res) => {
+  let task = tasks.find((i) => i.id === parseInt(req.params.id, 10));
+  if (!task) {
+    return res.status(404).send('Not found');
+  }
+  task.description = req.body.task;
+  res.redirect('/tasks');
+});
+
+// mark task as done
+router.get('/done/:id', (req, res) => {
+  let task = tasks.find((i) => i.id === parseInt(req.params.id, 10));
+  if (!task) {
+    return res.status(404).send('Not found');
+  }
+  task.done = true;
+  res.redirect('/tasks');
+});
+
+// mark task as not done
+router.get('/undo/:id', (req, res) => {
+  let task = tasks.find((i) => i.id === parseInt(req.params.id, 10));
+  if (!task) {
+    return res.status(404).send('Not found');
+  }
+  task.done = false;
+  res.redirect('/tasks');
+});
+
 // remove task
-router.post('/delete/:id', (req, res) => {
-  // find task and delete
+router.get('/delete/:id', (req, res) => {
+  let index = tasks.findIndex((i) => i.id === parseInt(req.params.id, 10));
+  if (!task) {
+    return res.status(404).send('Not found');
+  }
+  tasks.splice(index, 1);
+  res.redirect('/tasks');
 });
 
 module.exports = router;
